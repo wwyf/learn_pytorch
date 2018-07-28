686c5573ff2a2ca993208d3fb7be9d097630606c
walker <WalkerYF@yeah.net>
2018-07-28 10:46:41 +0800

    add 5-2

---
+++ pytorch-tutorial/5-word-embeddings.py
@@ -0,0 +1,15 @@
+# Author: Robert Guthrie
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
+
+word_to_ix = {"hello": 0, "world": 1}
+embeds = nn.Embedding(2, 5)  # 2 words in vocab, 5 dimensional embeddings
+lookup_tensor = torch.tensor([word_to_ix["hello"]], dtype=torch.long)
+print(lookup_tensor)
+hello_embed = embeds(lookup_tensor)
+print(hello_embed)
